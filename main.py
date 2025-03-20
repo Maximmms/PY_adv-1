e0a0d199fc04b0e05b195bd65d340f056ec642ca
Maxim Meleshkin <maximmms@hotmail.com>
2025-03-20 22:04:03 +0300

    first change

--- main.py
+++ main.py
@@ -5,5 +5,5 @@ from application.salary import calculate_salary
 
 if __name__ == '__main__':
     print(f'Сегодня: {date.today()}')
-    calculate_salary()
-    get_employee()
+    calculate_salary('Виктор')
+    get_employee('Alex', 26, 8999665441)
