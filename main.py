aef1441685ec4d7af569464bddafc28ef6581490
Maxim Meleshkin <maximmms@hotmail.com>
2025-03-20 22:02:21 +0300

    first change

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
 from datetime import date
 
+from application.db.people import get_employee
+from application.salary import calculate_salary
 
 if __name__ == '__main__':
     print(f'Сегодня: {date.today()}')
+    calculate_salary()
+    get_employee()
